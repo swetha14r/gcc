1bdf37d2fd9cc16de8dfd1b35152cb6bbd39defd
swetha <swetha1409r@gmail.com>
2024-12-28 16:42:31 +0530

    first commit

---
+++ gcc-react-app/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+import LandDetails from './LandDetails'
+import ResultScreen from './ResultScreen'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<LandDetails />} />
+          <Route path='/result' element={<ResultScreen />} />
+        </Routes>
+      </Router>
+
+    </>
+  )
+}
+
+export default App
