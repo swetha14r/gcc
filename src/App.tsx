dc1355c72880b6f681a6dfa16acd7bb348a6adad
swetha <swetha1409r@gmail.com>
2025-01-05 13:44:03 +0530

    5th

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import "./App.css";
 import LandDetails from "./LandDetails";
 import ResultScreen from "./ResultScreen";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Router>
         <Routes>
           <Route path="/" element={<LandDetails />} />
-          <Route path="./result" element={<ResultScreen />} />
+          <Route path="/result" element={<ResultScreen />} />
         </Routes>
       </Router>
+      </BrowserRouter>
     </>
   );
 }
