60be535b0d75ad17aee02d89b56c3cfe3f9ac6fd
swetha <swetha1409r@gmail.com>
2025-01-05 13:54:09 +0530

    i hate everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import "./App.css";
 import LandDetails from "./LandDetails";
 import ResultScreen from "./ResultScreen";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { HashRouter as Router, Routes, Route,} from "react-router-dom";
 
 function App() {
   return (
     <>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    
       <Router>
         <Routes>
           <Route path="/" element={<LandDetails />} />
           <Route path="/result" element={<ResultScreen />} />
         </Routes>
       </Router>
-      </BrowserRouter>
+      
     </>
   );
 }
